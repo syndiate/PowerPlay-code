c9b5610fb7b48077ec70da6dd5cebca8649d2fe6
Micah Jaffa <82616376+treelight100@users.noreply.github.com>
2022-11-15 16:09:37 -0500

    Fixed Color Values back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SleeveDetection.java
@@ -1,48 +1,148 @@
-package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
+        package org.firstinspires.ftc.teamcode;
 
-@Autonomous(name = "Signal Sleeve Test")
-public class VisionTest extends LinearOpMode {
+        import org.opencv.core.Core;
+        import org.opencv.core.CvType;
+        import org.opencv.core.Mat;
+        import org.opencv.core.Point;
+        import org.opencv.core.Rect;
+        import org.opencv.core.Scalar;
+        import org.opencv.core.Size;
+        import org.opencv.imgproc.Imgproc;
+        import org.openftc.easyopencv.OpenCvPipeline;
 
-    SleeveDetection sleeveDetection;
-    OpenCvCamera camera;
+public class SleeveDetection extends OpenCvPipeline {
+    /*
+    YELLOW  = Parking Left
+    CYAN    = Parking Middle
+    MAGENTA = Parking Right
+     */
 
-    // Name of the Webcam to be set in the config
-    String webcamName = "Webcam 1";
+    public enum ParkingPosition {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    // TOPLEFT anchor point for the bounding box
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+
+    // Width and height for the bounding box
+    public static int REGION_WIDTH = 30;
+    public static int REGION_HEIGHT = 50;
+
+    // Lower and upper boundaries for colors
+    private static final Scalar
+            lower_yellow_bounds  = new Scalar(96, 105, 51, 255),
+            upper_yellow_bounds  = new Scalar(249, 255, 187, 255),
+            lower_cyan_bounds    = new Scalar(57, 87, 140, 255),
+            upper_cyan_bounds    = new Scalar(172, 234, 255, 255),
+            lower_magenta_bounds = new Scalar(77, 21, 44, 255),
+            upper_magenta_bounds = new Scalar(201, 122, 189, 255);
+
+    // Color definitions
+    private final Scalar
+            YELLOW  = new Scalar(255, 255, 0),
+            CYAN    = new Scalar(0, 255, 255),
+            MAGENTA = new Scalar(255, 0, 0);
+
+    // Percent and mat definitions
+    private double yelPercent, cyaPercent, magPercent;
+    private Mat yelMat = new Mat(REGION_WIDTH, REGION_HEIGHT, CvType.CV_16UC4), cyaMat = new Mat(REGION_WIDTH, REGION_HEIGHT, CvType.CV_16UC4), magMat = new Mat(REGION_WIDTH, REGION_HEIGHT, CvType.CV_16UC4), blurredMat = new Mat(), kernel = new Mat();
+
+    // Anchor point definitions
+    Point sleeve_pointA = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y);
+    Point sleeve_pointB = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+    // Running variable storing the parking position
+    private volatile ParkingPosition position = ParkingPosition.LEFT;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
-        sleeveDetection = new SleeveDetection();
-        camera.setPipeline(sleeveDetection);
+    public Mat processFrame(Mat input) {
+        // Noise reduction
+        Imgproc.blur(input, blurredMat, new Size(5, 5));
+        blurredMat = blurredMat.submat(new Rect(sleeve_pointA, sleeve_pointB));
 
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                camera.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
-            }
+        // Apply Morphology
+        kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+        Imgproc.morphologyEx(blurredMat, blurredMat, Imgproc.MORPH_CLOSE, kernel);
 
-            @Override
-            public void onError(int errorCode) {}
-        });
+        // Gets channels from given source mat
+        Core.inRange(blurredMat, lower_yellow_bounds, upper_yellow_bounds, yelMat);
+        Core.inRange(blurredMat, lower_cyan_bounds, upper_cyan_bounds, cyaMat);
+        Core.inRange(blurredMat, lower_magenta_bounds, upper_magenta_bounds, magMat);
 
-        while (!isStarted()) {
-            telemetry.addData("YCM: ", sleeveDetection.getYelPercent() + " " +
-                    sleeveDetection.getCyaPercent() + " " + sleeveDetection.getMagPercent());
-            telemetry.addData("ROTATION1: ", sleeveDetection.getPosition());
-            telemetry.update();
+        //Imgproc.cvtColor(input.rgba());
+        // Gets color specific values
+
+        yelPercent = Core.countNonZero(yelMat);
+        cyaPercent = Core.countNonZero(cyaMat);
+        magPercent = Core.countNonZero(magMat);
+
+        // Calculates the highest amount of pixels being covered on each side
+        double maxPercent = Math.max(yelPercent, Math.max(cyaPercent, magPercent));
+
+        // Checks all percentages, will highlight bounding box in camera preview
+        // based on what color is being detected
+        if (maxPercent == yelPercent) {
+            position = ParkingPosition.LEFT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    YELLOW,
+                    2
+            );
+        } else if (maxPercent == cyaPercent) {
+            position = ParkingPosition.CENTER;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    CYAN,
+                    2
+            );
+        } else if (maxPercent == magPercent) {
+            position = ParkingPosition.RIGHT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    MAGENTA,
+                    2
+            );
         }
 
-        waitForStart();
+        // Memory cleanup
+        blurredMat.release();
+        yelMat.release();
+        cyaMat.release();
+        magMat.release();
+        kernel.release();
+
+        return input;
     }
+
+    // Returns an enum being the current position where the robot will park
+    public ParkingPosition getPosition() {
+        return position;
+    }
+
+    public double getYelPercent() {
+        return yelPercent;
+    }
+
+    public double getCyaPercent() {
+        return cyaPercent;
+    }
+
+    public double getMagPercent() {
+        return magPercent;
+    }
+
 }
