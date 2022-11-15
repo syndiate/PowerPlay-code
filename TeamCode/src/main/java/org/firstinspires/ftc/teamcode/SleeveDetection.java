48cd776f84010da685bbee312b0a135c93149d2c
Micah Jaffa <82616376+treelight100@users.noreply.github.com>
2022-11-15 16:08:48 -0500

    Changed back Color Values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SleeveDetection.java
@@ -1,131 +1,48 @@
-
-
 package org.firstinspires.ftc.teamcode;
 
-import org.opencv.core.Core;
-        import org.opencv.core.CvType;
-        import org.opencv.core.Mat;
-        import org.opencv.core.Point;
-        import org.opencv.core.Rect;
-        import org.opencv.core.Scalar;
-        import org.opencv.core.Size;
-        import org.opencv.imgproc.Imgproc;
-        import org.openftc.easyopencv.OpenCvPipeline;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class SleeveDetection extends OpenCvPipeline {
-    /*
-    YELLOW  = Parking Left
-    CYAN    = Parking Middle
-    MAGENTA = Parking Right
-     */
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-    public enum ParkingPosition {
-        LEFT,
-        CENTER,
-        RIGHT
-    }
+@Autonomous(name = "Signal Sleeve Test")
+public class VisionTest extends LinearOpMode {
 
-    // TOPLEFT anchor point for the bounding box
-    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+    SleeveDetection sleeveDetection;
+    OpenCvCamera camera;
 
-    // Width and height for the bounding box
-    public static int REGION_WIDTH = 30;
-    public static int REGION_HEIGHT = 50;
-
-    // Lower and upper boundaries for colors
-    private static final Scalar
-            lower_red_bounds  = new Scalar(114, 20, 36, 255),
-            upper_red_bounds  = new Scalar(255, 13, 53, 255),
-            //lower_cyan_bounds    = new Scalar(57, 87, 140, 255),
-            //upper_cyan_bounds    = new Scalar(172, 234, 255, 255),
-            lower_blue_bounds = new Scalar(13, 20, 80, 255),
-            upper_blue_bounds = new Scalar(46, 137, 255, 255);
-
-    // Color definitions
-    private final Scalar
-            RED  = new Scalar(255, 0, 0),
-            BLUE    = new Scalar(0, 0, 255);
-
-    // Percent and mat definitions
-    private double redPercent, bluePercent;
-    private Mat redMat = new Mat(REGION_WIDTH, REGION_HEIGHT, CvType.CV_16UC4), blueMat = new Mat(REGION_WIDTH, REGION_HEIGHT, CvType.CV_16UC4), blurredMat = new Mat(), kernel = new Mat();
-
-    // Anchor point definitions
-    Point sleeve_pointA = new Point(
-            SLEEVE_TOPLEFT_ANCHOR_POINT.x,
-            SLEEVE_TOPLEFT_ANCHOR_POINT.y);
-    Point sleeve_pointB = new Point(
-            SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-    // Running variable storing the parking position
-    private volatile ParkingPosition position = ParkingPosition.LEFT;
+    // Name of the Webcam to be set in the config
+    String webcamName = "Webcam 1";
 
     @Override
-    public Mat processFrame(Mat input) {
-        // Noise reduction
-        Imgproc.blur(input, blurredMat, new Size(5, 5));
-        blurredMat = blurredMat.submat(new Rect(sleeve_pointA, sleeve_pointB));
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+        sleeveDetection = new SleeveDetection();
+        camera.setPipeline(sleeveDetection);
 
-        // Apply Morphology
-        kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
-        Imgproc.morphologyEx(blurredMat, blurredMat, Imgproc.MORPH_CLOSE, kernel);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
 
-        // Gets channels from given source mat
-        Core.inRange(blurredMat, lower_red_bounds, upper_red_bounds, redMat);
-        Core.inRange(blurredMat, lower_blue_bounds, upper_blue_bounds, blueMat);
+            @Override
+            public void onError(int errorCode) {}
+        });
 
-        //Imgproc.cvtColor(input.rgba());
-        // Gets color specific values
-
-        redPercent = Core.countNonZero(redMat);
-        bluePercent = Core.countNonZero(blueMat);
-
-        // Calculates the highest amount of pixels being covered on each side
-        double maxPercent = Math.max(redPercent, bluePercent);
-
-        // Checks all percentages, will highlight bounding box in camera preview
-        // based on what color is being detected
-        if (maxPercent == redPercent) {
-            position = ParkingPosition.LEFT;
-            Imgproc.rectangle(
-                    input,
-                    sleeve_pointA,
-                    sleeve_pointB,
-                    RED,
-                    2
-            );
-        } else if (maxPercent == bluePercent) {
-            position = ParkingPosition.CENTER;
-            Imgproc.rectangle(
-                    input,
-                    sleeve_pointA,
-                    sleeve_pointB,
-                    BLUE,
-                    2
-            );
+        while (!isStarted()) {
+            telemetry.addData("YCM: ", sleeveDetection.getYelPercent() + " " +
+                    sleeveDetection.getCyaPercent() + " " + sleeveDetection.getMagPercent());
+            telemetry.addData("ROTATION1: ", sleeveDetection.getPosition());
+            telemetry.update();
         }
 
-        // Memory cleanup
-        blurredMat.release();
-        redMat.release();
-        blueMat.release();
-        kernel.release();
-
-        return input;
+        waitForStart();
     }
-
-    // Returns an enum being the current position where the robot will park
-    public ParkingPosition getPosition() {
-        return position;
-    }
-
-    public double getRedPercent() {
-        return redPercent;
-    }
-
-    public double getBluePercent() {
-        return bluePercent;
-    }
-
 }
