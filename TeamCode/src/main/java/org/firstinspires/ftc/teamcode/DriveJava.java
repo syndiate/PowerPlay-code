428ec2cb805d6e2f64cca29fdac50902e73a87c9
Micah Jaffa <82616376+treelight100@users.noreply.github.com>
2023-01-19 07:13:33 -0500

    meet day changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveJava.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "JavaDrive")
 public class DriveJava extends LinearOpMode {
-
     private DcMotor right_drive1;
     private DcMotor right_drive2;
     private DcMotor left_drive1;
@@ -16,13 +15,12 @@ public class DriveJava extends LinearOpMode {
     private DcMotor lift;
     private Servo claw1;
     private Servo claw2;
-    private double powerFactor = 0;
-    private int speedChangeValue = 1;
-    private boolean speedChangedUp = false;
-    private boolean speedChangedDown = false;
+    double powerFactor = 0;
+    int speedChangeValue = 1;
+    boolean speedChangedUp = false;
+    boolean speedChangedDown = false;
 
     private void initMotors() {
-
         right_drive1 = hardwareMap.get(DcMotor.class, "right_drive1");
         right_drive2 = hardwareMap.get(DcMotor.class, "right_drive2");
         left_drive1 = hardwareMap.get(DcMotor.class, "left_drive1");
@@ -36,7 +34,7 @@ public class DriveJava extends LinearOpMode {
         lift.setDirection(DcMotorSimple.Direction.FORWARD);
         powerFactor = 0.4;
         claw1.setPosition(0.25);
-        claw2.setPosition(0.65);
+        claw2.setPosition(0.75);
 
     }
 
@@ -51,27 +49,27 @@ public class DriveJava extends LinearOpMode {
          * Wait for the user to press start on the Driver Station
          */
         waitForStart();
-        telemetry.addLine("Driver mode initiated");
-        telemetry.update();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            telemetry.update();
+            while (opModeIsActive()) {
 
-        while (opModeIsActive()) {
+                callSpeedChange();
+                lift.setPower((gamepad2.right_trigger - gamepad2.left_trigger) * 0.5);
 
-            callSpeedChange();
-            lift.setPower((gamepad2.right_trigger - gamepad2.left_trigger) * 0.5);
-            clawBot(gamepad2.right_bumper, gamepad2.left_bumper);
-            moveBot(-gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
-
+                clawBot(gamepad2.right_bumper, gamepad2.left_bumper);
+                moveBot(-gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
+                // Put loop blocks here.
+            }
         }
     }
 
     /**
      * Describe this function...
      */
-    
     void callSpeedChange()
     {
-
-        if (this.gamepad1.right_trigger > 0 && !speedChangedUp)
+        if(this.gamepad1.right_trigger > 0 && !speedChangedUp)
         {
             speedChangedUp = true;
             speedChange(true);
@@ -89,7 +87,6 @@ public class DriveJava extends LinearOpMode {
         {
             speedChangedDown = false;
         }
-
     }
 
     void speedChange(boolean faster)
@@ -133,19 +130,17 @@ public class DriveJava extends LinearOpMode {
         right_drive2.setPower(powerFactor * (-pivot + vertical + horizontal));
         left_drive1.setPower(powerFactor * (pivot + vertical + horizontal));
         left_drive2.setPower(powerFactor * (pivot + (vertical - horizontal)));
-
         right_drive1.setPower(0);
         right_drive2.setPower(0);
         left_drive1.setPower(0);
         left_drive2.setPower(0);
-
     }
 
     private void clawBot(boolean right_bumper, boolean left_bumper)
     {
         if (right_bumper)
         {
-            claw1.setPosition(0.15);
+            claw1.setPosition(0.25);
             claw2.setPosition(0.65);
         }
         else if (left_bumper)
