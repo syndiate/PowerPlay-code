cc8b42bb3f9fdf6e2a220366b8e0ec859fe6d44f
Micah Jaffa <82616376+treelight100@users.noreply.github.com>
2023-01-19 07:13:01 -0500

    Meet day changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoJavaConeLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoJavaConeLeft.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-
 @Autonomous(name = "AutoJavaConeLeft", group = "Auto")
 public class AutoJavaConeLeft extends AutoJavaCone {
 
@@ -13,60 +12,75 @@ public class AutoJavaConeLeft extends AutoJavaCone {
         initMotors();
         initCamera();
 
-        while (!isStarted()) updateParkingPos();
-        camera.closeCameraDevice();
-
+        while (!isStarted()) {
+            telemetry.addData("YCM: ", sleeveDetection.getYelPercent() + " " +
+                    sleeveDetection.getCyaPercent() + " " + sleeveDetection.getMagPercent());
+            telemetry.addData("ROTATION1: ", sleeveDetection.getPosition());
+            telemetry.update();
+            pos = sleeveDetection.getPosition();
+        }
 
         telemetry.addLine("Waiting for start");
         telemetry.update();
         waitForStart();
 
         startPressed = true;
-        if (!opModeIsActive()) return;
+        boolean stop = false;
+        camera.closeCameraDevice();
 
-        /*
-         moveBot(95, 45, false, false);
-                telemetry.addData("x:", position.get(0));
-                telemetry.addData("y:", position.get(1));
+        while (opModeIsActive())
+        {
+            if (!stop) {
+                telemetry.addData("clawClosed", clawClosed);
                 telemetry.update();
-                sleep(2000);
-                moveBot(101, 18, false, true);
-                telemetry.addData("x:", position.get(0));
-                telemetry.addData("y:", position.get(1));
-                telemetry.update();
-                */
-        clawBot();
-        //liftCone(0);
-        moveBot(32, 1, 0, 0);
-        moveBot(4, -1, 0, 0);
-        sleep(1000);
-        moveBot(17, 0, 0, 1);
-        //liftCone(2);
-        powerFactor = 0.25;
-        //moveBot(5, 1, 0, 0);
-        sleep(1000);
-        //liftCone(1);
-        sleep(500);
-        //clawBot();
-        //moveBot(2, -1, 0, 0);
-        //liftCone(-1);
-        powerFactor = startingPF;
-        switch (pos) {
-            case RIGHT: {
+                clawBot();
+                powerFactor = 0.7;
+                startingPF = powerFactor;
+                sleep(1000);
+                liftCone(-1);
+                moveBot(2, 0, 0, 1);
+                moveBot(35, 1, 0, 0);
+                moveBot(5, 1, 0, 0);
+                sleep(500);
+                moveBot(1.5f, -1, 0, 0);
+                //moveBot(4, -1, 0, 0);
+                sleep(1000);
+                moveBot(56.5f, 0, 0, 1);
+                liftCone(3);
+                powerFactor = 0.25;
+                moveBot(5, 1, 0, 0);
+                sleep(1000);
+//                liftCone(1);
+                sleep(500);
+                clawBot();
+                moveBot(2, -1, 0, 0);
+                liftCone(-1);
+//                liftCone(-1);
+                powerFactor = startingPF;
+                switch (pos) {
+                    case RIGHT: {
+ //                       moveBot(1, 1, 0, 0);
+                        moveBot(10, 0, 0, -1);
+                        break;
+                    }
+                    case CENTER: {
+                        moveBot(9, 0, 0, -1);
+                        moveBot(1, -1, 0, 0);
+                        moveBot(43, 0, 0, -1);
+                        break;
+                    }
+                    case LEFT: {
+                        moveBot(9, 0, 0, -1);
+                        moveBot(1, -1, 0, 0);
+                        moveBot(86, 0, 0, -1);
+                        break;
+                    }
+                }
                 moveBot(1, 1, 0, 0);
-                moveBot(11, 0, 0, 1);
-                break;
+                stop = true;
             }
-            case CENTER: {
-                moveBot(13, 0, 0, -1);
-                break;
-            }
-            case LEFT: {
-                moveBot(43, 0, 0, -1);
-                break;
-            }
-        }
 
+        }
 
 
     }
